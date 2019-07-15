2f3dadf0c547d0322537939c10358c235e9d651f
Takayuki Kaburagi <t.kaburagi@me.com>
2019-07-15 21:24:02 +0900

    something

--- set-env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export VAULT_HOST=cf-vault-kabuctl.cfapps.io
-export VAULT_TOKEN=s.PIaA1xSvI5er37mYnDnthN6D
-export APPROLE=cf-vault-approle
-export MYSQL_JDBC_URL=jdbc:mysql://us-cdbr-iron-east-02.cleardb.net/ad_284c40e550e4890
-export MYSQL_USERNAME=bf3251e305b28f
-export MYSQL_PASSWORD=26f8e902
