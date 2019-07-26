1c48d99971a4797dde25aaacd6523cc802105270
Takayuki Kaburagi <t.kaburagi@me.com>
2019-07-25 19:24:24 -0700

    Delete my-set-env-pws.sh

--- my-set-env-pws.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export VAULT_HOST=cf-vault-kabu.cfapps.io
-export VAULT_TOKEN=s.PIaA1xSvI5er37mYnDnthN6D
-export VAULT_PORT=443
-export APPROLE=vault-approle
-export MYSQL_JDBC_URL=jdbc:mysql://127.0.0.1:3306/handson
