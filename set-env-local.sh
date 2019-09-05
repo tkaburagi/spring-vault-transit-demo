a7f72f80aa4f305440ee3e07c5b0f61344b1405b
Takayuki Kaburagi <t.kaburagi@me.com>
2019-09-05 12:08:53 +0900

    Update set-env-local.sh

--- set-env-local.sh
+++ set-env-local.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export VAULT_HOST=127.0.0.1
-export VAULT_TOKEN=s.M8D76JOdWrjd81we2CTnj8Zw
+export VAULT_TOKEN=
 export VAULT_PORT=8200
 export APPROLE=vault-approle
 export MYSQL_JDBC_URL=jdbc:mysql://127.0.0.1:3306/handson
